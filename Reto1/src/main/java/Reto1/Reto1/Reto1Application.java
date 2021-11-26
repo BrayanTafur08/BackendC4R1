8dd97c1b0fe1e6793494596f0e860c0e8c055163
Brayan <Reto_2_Funcional>
2021-11-25 20:17:00 -0500

    first commit

---
+++ Reto1/src/main/java/Reto1/Reto1/Reto1Application.java
@@ -0,0 +1,19 @@
+//Modelo user
+//Interface user crud reposirory
+//Repositorio userrepository
+//Servicio userService
+//Controlador UserController
+
+package Reto1.Reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Application.class, args);
+	}
+
+}
